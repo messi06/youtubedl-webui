a86ae0b9067d9c65603e663f47eac7e66e72693d
Phil <phil@ucode.space>
2020-03-31 21:26:59 +0200

    beta testing

--- main.py
+++ main.py
@@ -7,16 +7,49 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-cle
+
 @app.route('/download', methods=['POST'])
 def dwn():
-    url = request.form['name']
+    error = 0
+    url = request.form['url']
     ext = request.form['ext']
-    # if m.youtube.com or youtu.be change it
-    name = url[32:43]
-    system('youtube-dl -o "'+name+ext+'"')
-return send_file('/yt/'+name+ext, as_attachment=True)
+    qal = request.form['qal']
+
+    if (url[0:16] == "https://www.yout"):
+        name = url[32:43]
+    elif (url[0:16] == "https://youtube."):
+        name = url[28:39]
+    elif (url[0:16] == "https://m.youtub"):
+        name = url[30:41]
+    elif (url[0:16] == "https://youtu.be"):
+        name = url[17:28]
+    else:
+        error = 1
+
+    if (ext == "mp3"):
+        system('youtube-dl --extract-audio --add-metadata --audio-format "mp3" --embed-thumbnail -o "/yt/'+name+'.%(ext)s" '+url)
+    elif (ext == "mp4"):
+        if(qal == "720"):
+            system('youtube-dl --add-metadata -f "best[height<=720]" --recode-video mp4 -o "/yt/'+name+'.%(ext)s" '+url)
+        elif(qal == "1080"):
+            system('youtube-dl --add-metadata -f "best[height<=1080]" --recode-video mp4 -o "/yt/'+name+'.%(ext)s" '+url)
+        elif(qal == "9999"):
+            system('youtube-dl --add-metadata -f "best" --recode-video mp4 -o "/yt/'+name+'.%(ext)s" '+url)
+        else:
+            error = 1
+    else:
+        error = 1
+    
+    if(error == 0):
+        ret = send_file('/yt/'+name+'.'+ext, as_attachment=True)
+    else:
+        ret = render_template("error.html")
+
+    return ret
 
 if __name__ == '__main__':
-    app.run()
-#  youtube-dl -o 1.%(ext)s
+    app.run(host="0.0.0.0")
+
+# youtube-dl -o 1.%(ext)s
+# -f "bestvideo[height<=720]+bestaudio/best[height<=720]" --merge-output-format mp4
+# --merge-output-format mp4
